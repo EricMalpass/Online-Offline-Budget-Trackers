619cdc5068ecbd773a8ebb9f1c3a8bdac096bb0f
EricMalpass <ericmalpass@gmail.com>
2021-06-11 16:56:59 -0700

    it's working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-console.log("Hi from your service-worker.js file!");
+//console.log("Hi from your service-worker.js file!");
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -6,18 +6,35 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "/styles.css"
+  "/styles.css",
+  "/db.js",
+  "/manifest.webmanifest",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
+
   // install
-self.addEventListener("install", function (evt) {
-    // pre cache all static assets
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
-  });
+// self.addEventListener("install", function (evt) {
+//     // pre cache all static assets
+//     evt.waitUntil(
+//       caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+//     );
+//     // tell the browser to activate this service worker immediately once it
+//     // has finished installing
+//   });
+
+const PRECACHE = 'precache-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
 
   // fetch
 self.addEventListener("fetch", function(evt) {
