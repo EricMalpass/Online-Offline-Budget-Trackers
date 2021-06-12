5dcb993cfb36360ba2b06390316030287ca40d0e
EricMalpass <ericmalpass@gmail.com>
2021-06-12 13:11:53 -0700

    test

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Transaction',
+  process.env.MONGODB_URI || 'mongodb://localhost/shrouded-plains',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
